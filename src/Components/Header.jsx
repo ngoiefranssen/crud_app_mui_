f745d49e1efa74ed75eb6238104f64bee1722300
Franssen <ngoiefranssen04@gmail.com>
2023-01-18 08:51:12 +0200

    Wip

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const typText = styled(Typography)`
-    font-size:
+    font-size : 20px;
+    margin-right : 20px;
 `
 
 const Header = () => {
