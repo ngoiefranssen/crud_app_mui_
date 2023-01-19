9ae25e107383f7e409dc38425d171fba13747bf1
Franssen <ngoiefranssen04@gmail.com>
2023-01-19 17:13:58 +0200

    Wip

---
+++ src/Pagination/usePagination.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const usePagination = (records, headCells, filter) => {
+
+    const pages = [5, 10, 25];
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(pages[page]);
+    const [order, setOrder] = React.useState();
+    const [orderBy, setOrderBy] = React.useState();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default usePagination
